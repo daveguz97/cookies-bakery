ee2709f38d91c9da371cd21405255f146152e847
David Guzman <davidguzmaninc@gmail.com>
2020-07-22 14:45:10 -0400

    Added initial setup

---
+++ src/components/landing-page/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <section className="LandingPage">
+      <div className="container">
+        <h1>Cookie's Bakery</h1>
+        <p className="lead">Best Bakery in North Carolina</p>
+      </div>
+    </section>
+  );
+}
